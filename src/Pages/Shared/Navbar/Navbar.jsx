c17bbb3a4fa471591db5d5fc08c8be5009933187
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-24 19:27:37 +0600

    set navbar position, bg, and text color

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 max-w-screen-xl bg-opacity-30 bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
