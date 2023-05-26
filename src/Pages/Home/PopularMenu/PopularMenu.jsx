a24bc4b70eed03b59108b13d46fe16bd0c649ebd
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-26 14:08:59 +0600

    added menu in navabr

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -29,6 +29,9 @@ const PopularMenu = () => {
                     ></MenuItem>)
                 }
             </div>
+            <div className="text-center mt-16">
+                <button className="btn btn-outline border-0 border-b-4 ">View Full Menu</button>
+            </div>
         </section>
     );
 };
---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+
+
+const Menu = () => {
+    return (
+        <div>
+            <h2>menu</h2>
+        </div>
+    );
+};
+
+export default Menu;
