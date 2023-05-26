a54a147220b8b647d3e7993c29c6d5bac0732504
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-26 14:30:13 +0600

    added dynamic page title by route using react-helmet

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,11 @@
-
+import { Helmet } from 'react-helmet-async';
 
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
             <h2>menu</h2>
         </div>
     );
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
     const navOptions = <>
+        <li><Link to='/'>Home</Link></li>
         <li><Link to='/menu'>Our Menu</Link></li>
-      
-        <li><a>Item 3</a></li>
+     
     </>
 
     return (
