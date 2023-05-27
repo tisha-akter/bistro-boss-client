d204f00dc4063794f5dfb2a8773a06e05672ca9d
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-27 20:59:18 +0600

    added coverphoto in orderfood page

---
+++ src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,12 @@
+import oderCover from '../../../assets/shop/order.jpg';
+import Cover from '../../Shared/Cover/Cover';
+
+const Order = () => {
+    return (
+        <div>
+            <Cover img={oderCover} title="Order Food"></Cover>
+        </div>
+    );
+};
+
+export default Order;
