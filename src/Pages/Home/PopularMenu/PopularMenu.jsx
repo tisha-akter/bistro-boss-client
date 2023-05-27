c8921627307e2db8cfa495d16a3e38a5d2914a08
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-27 16:41:56 +0600

    showed popular item by using custom hook

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,19 +1,13 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import UseMenu from "../../../hooks/UseMenu";
 
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
 
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const PopularItems = data.filter(item => item.category === 'popular')
-                setMenu(PopularItems);
-            })
-    }, [])
+    const [ menu ] = UseMenu();
+    const popular = menu.filter(item => item.category === 'popular');
+
     return (
         <section className="mb-12">
             <SectionTitle
@@ -23,7 +17,7 @@ const PopularMenu = () => {
             </SectionTitle>
             <div className="grid md:grid-cols-2 gap-10">
                 {
-                    menu.map(item => <MenuItem
+                   popular.map(item => <MenuItem
                         key={item._id}
                         item={item}
                     ></MenuItem>)
