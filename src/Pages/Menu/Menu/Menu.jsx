163d28e6f720326bf4a8ebbbcb376fc72ad6d9d7
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-27 11:53:57 +0600

    added implemnet cover with blur parallax using react parallax

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -6,7 +9,21 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>menu</h2>
+            <Cover
+                img={menuImg}
+                title='Our menu'
+            ></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover
+                img={menuImg}
+                title='Our menu'
+            ></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover
+                img={menuImg}
+                title='Our menu'
+            ></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
