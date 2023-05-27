51e322af99ef02aeb0096276fe7baab50d70b137
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-27 21:19:22 +0600

    added react tab in order.jsx page used UseMenu hook

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react';
 import oderCover from '../../../assets/shop/order.jpg';
 import Cover from '../../Shared/Cover/Cover';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import UseMenu from '../../../hooks/UseMenu';
+
 
 const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = UseMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const pizzas = menu.filter(item => item.category === 'pizza');
+    const salads = menu.filter(item => item.category === 'salad');
+    const soups = menu.filter(item => item.category === 'soup');
+    const offered = menu.filter(item => item.category === 'offered');
+   
     return (
         <div>
             <Cover img={oderCover} title="Order Food"></Cover>
+
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drink</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
