ccde97faffa9de7a22f64023ba527e28b8cc1bf1
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-27 16:30:56 +0600

    added custom hook Usemenu to load menu data

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,9 +1,12 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg';
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 
 const Menu = () => {
+
+    
+
     return (
         <div>
             <Helmet>
@@ -13,17 +16,8 @@ const Menu = () => {
                 img={menuImg}
                 title='Our menu'
             ></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover
-                img={menuImg}
-                title='Our menu'
-            ></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover
-                img={menuImg}
-                title='Our menu'
-            ></Cover>
-            <PopularMenu></PopularMenu>
+            
+           
         </div>
     );
 };
