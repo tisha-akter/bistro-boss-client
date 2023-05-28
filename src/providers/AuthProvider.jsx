f7987edc9c6663813c7e0402e8b6d95501d9e263
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-29 03:05:07 +0600

    added logOut, sweet alret and change recaptcha settng

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -18,7 +18,7 @@ const AuthProvider = ({ children }) => {
 
     const login = (email, password) => {
         setLoading(true);
-        return signInWithEmailAndPassword(email, password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
 
     const logOut = () => {
