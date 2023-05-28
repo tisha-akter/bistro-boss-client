4a27467d28009ebe8d003648eabcb2aa7f12a36a
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-28 16:26:35 +0600

    changed food card's button style

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,9 +1,9 @@
 
 
-const FoodCard = ({item}) => {
-    const {image, name, recipe,  price} = item;
+const FoodCard = ({ item }) => {
+    const { image, name, recipe, price } = item;
     return (
-        
+
         <div className="card w-96 bg-base-100 shadow-xl ">
             <figure><img src={image} alt="Shoes" /></figure>
             <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
@@ -11,7 +11,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className="btn btn-outline bg-slate-100 border-0 border-orange-400 border-b-4 mt-10">Add to Cart</button>
                 </div>
             </div>
         </div>
