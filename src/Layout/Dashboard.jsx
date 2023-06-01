62f0c4d380cc289f2f45952f7673f433cfaaea1b
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-01 22:30:07 +0600

    added delete cart item and sync data using refetch

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,8 +1,10 @@
 import { FaCalendar, FaHome, FaShoppingCart, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -20,13 +22,20 @@ const Dashboard = () => {
                     <li><NavLink to='/dashboard/home'><FaHome></FaHome>User Home</NavLink></li>
                     <li><NavLink to='/dashboard/reservations'><FaCalendar></FaCalendar>Reservations</NavLink></li>
                     <li><NavLink to='/dashboard/payhistory'><FaWallet></FaWallet>Payment   History</NavLink></li>
-                    <li><NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart</NavLink></li>
+
+                    <li>
+                        <NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart
+                            <span className="badge badge-secondary">+{cart?.length || 0}</span>
+                        </NavLink>
+                    </li>
+
+
                     <div className="divider"></div>
                     <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
                     <li><NavLink to='/menu'></NavLink></li>
                     <li><NavLink></NavLink></li>
                     <li><NavLink></NavLink></li>
-                   
+
 
 
                 </ul>
--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,14 +1,49 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const MyCart = () => {
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
     // console.log(cart);
+
+    // how does reduce work
     const total = cart.reduce((sum, item) => item.price + sum, 0)
+
+    const handleDelete = item => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              fetch(`http://localhost:5000/carts/${item._id}`, {
+                method: 'Delete'
+
+              })
+              .then(res => res.json())
+              .then(data => {
+                if (data.deletedCount>0){
+                    refetch();
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                      )
+                }
+              })
+            }
+          })
+    }
+
+
     return (
-        <div>
+        <div className="w-full">
             <Helmet>
                 <title>Bistro Boss | My Cart</title>
             </Helmet>
@@ -57,7 +92,7 @@ const MyCart = () => {
                                 </td>
                                 <td className="text-end">${item.price}</td>
                                 <td>
-                                    <button className="btn btn-ghost bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
+                                    <button onClick={ () => handleDelete(item)} className="btn btn-ghost bg-red-600 text-white"><FaTrashAlt></FaTrashAlt></button>
                                 </td>
                             </tr>)
                         }
