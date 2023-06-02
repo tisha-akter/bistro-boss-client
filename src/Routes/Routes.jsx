d70b37f10d9cf42f685a78dfea708cfb36aabd08
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-02 13:11:51 +0600

    convert privateRoute to the dashboard

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -45,7 +45,7 @@ import MyCart from "../Pages/Dashboard/MyCart/MyCart";
     },
     {
       path: 'dashboard',
-      element: <Dashboard></Dashboard>,
+      element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
       children: [
         {
           path: 'mycart',
