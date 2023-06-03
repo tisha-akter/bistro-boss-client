a10d82a2b6a19fad86953fabaf0c118e5b7084b0
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-03 12:29:54 +0600

    loaded all users on the dashboard page

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,10 +1,15 @@
-import { FaCalendar, FaHome, FaShoppingCart, FaWallet } from "react-icons/fa";
+import { FaBook, FaCalendar, FaHome, FaShoppingCart, FaUsers, FaUtensils, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
+
+    // todo : load data from the server to have dynamic isAdmin based on data 
+    const isAdmin = true;
+
+
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -17,17 +22,31 @@ const Dashboard = () => {
             </div>
             <div className="drawer-side bg-[#D1A054]">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 text-base-content">
+                <ul className="menu p-4 w-80">
 
-                    <li><NavLink to='/dashboard/home'><FaHome></FaHome>User Home</NavLink></li>
-                    <li><NavLink to='/dashboard/reservations'><FaCalendar></FaCalendar>Reservations</NavLink></li>
-                    <li><NavLink to='/dashboard/payhistory'><FaWallet></FaWallet>Payment   History</NavLink></li>
+                    {
+                        isAdmin ? <>
+                            <li><NavLink to='/dashboard/home'><FaHome></FaHome>Admin Home</NavLink></li>
+                            <li><NavLink to='/dashboard/reservations'><FaUtensils></FaUtensils> Add Items</NavLink></li>
+                            <li><NavLink to='/dashboard/payhistory'> Manage Items</NavLink></li>
+                            <li><NavLink to='/dashboard/payhistory'><FaBook></FaBook> Manage Bookings</NavLink></li>
+                            <li><NavLink to='/dashboard/allusers'><FaUsers></FaUsers>All Users</NavLink></li>
 
-                    <li>
-                        <NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart
-                            <span className="badge badge-secondary">+{cart?.length || 0}</span>
-                        </NavLink>
-                    </li>
+                        </> : <>
+
+                            <li><NavLink to='/dashboard/home'><FaHome></FaHome>User Home</NavLink></li>
+                            <li><NavLink to='/dashboard/reservations'><FaCalendar></FaCalendar>Reservations</NavLink></li>
+                            <li><NavLink to='/dashboard/payhistory'><FaWallet></FaWallet>Payment   History</NavLink></li>
+
+                            <li>
+                                <NavLink to='/dashboard/mycart'><FaShoppingCart></FaShoppingCart>My Cart
+                                    <span className="badge badge-secondary">+{cart?.length || 0}</span>
+                                </NavLink>
+                            </li>
+                        </>
+                    }
+
+
 
 
                     <div className="divider"></div>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./PrivateRoute";
 import Secret from "../Pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import MyCart from "../Pages/Dashboard/MyCart/MyCart";
+import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 
  export const router = createBrowserRouter([
     {
@@ -50,6 +51,10 @@ import MyCart from "../Pages/Dashboard/MyCart/MyCart";
         {
           path: 'mycart',
           element: <MyCart></MyCart>
+        },
+        {
+          path: 'allusers',
+          element: <AllUsers></AllUsers>
         }
       ]
     }
