c94ae1dc3f9884309c56151b23c27fa41805c263
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-04 12:59:31 +0600

    added axios to load data instead of fetch

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -44,6 +45,16 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser);
+
+            // get and set token 
+            if(currentUser) {
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email })
+            .then(data => {
+                console.log(data)
+            })
+            }
+            
+
             setLoading(false);
         });
         return () => {
