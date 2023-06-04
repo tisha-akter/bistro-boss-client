7b85fce805e69350c4dde8ef2deab531d4feb3ef
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-04 15:40:59 +0600

    added useAdmin hook

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,13 +1,16 @@
 import { FaBook, FaCalendar, FaHome, FaShoppingCart, FaUsers, FaUtensils, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
 
     // todo : load data from the server to have dynamic isAdmin based on data 
-    const isAdmin = true;
+    // const isAdmin = true;
+
+    const [isAdmin] = useAdmin();
 
 
     return (
