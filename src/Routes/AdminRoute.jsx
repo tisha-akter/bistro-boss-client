1c29816d325e40585f7dbe70fa70e2dae98918a3
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-04 16:27:20 +0600

    created adminRoute

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,29 @@
+
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import useAdmin from "../hooks/useAdmin";
+
+
+const AdminRoute = ({children}) => {
+    const {user, loading} = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if(loading || isAdminLoading){
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400">
+                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white"></div>
+                </div>
+            </div>
+        );
+    }
+
+
+    if (user && isAdmin) {
+        return children;
+    }
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>
+};
+
+export default AdminRoute;
