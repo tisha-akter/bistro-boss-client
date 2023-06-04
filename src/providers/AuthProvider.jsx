74476579dd4f2ac74c6d6e020439f7a95986dc9a
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-04 13:08:54 +0600

    set token in localstorage and remove it when user isn't available

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -50,10 +50,14 @@ const AuthProvider = ({ children }) => {
             if(currentUser) {
                 axios.post('http://localhost:5000/jwt', {email: currentUser.email })
             .then(data => {
-                console.log(data)
+                console.log(data.data.token)
+                localStorage.setItem('access-token', data.data.token)
             })
             }
-            
+            else{
+                localStorage.removeItem('access-token')
+            }
+
 
             setLoading(false);
         });
