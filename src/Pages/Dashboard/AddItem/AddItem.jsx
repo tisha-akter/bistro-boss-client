fb200b99b51ff0c2a0a830e85b82185531773a38
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-05 17:47:30 +0600

    upload image to hosting server imgbb and get image url

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,13 +1,30 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
 
+const img_hosting_token=import.meta.env.VITE_Image_Upload_Token;
+
 const AddItem = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const img_hosting_url =`https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`
+
     const onSubmit = data => {
+       
         console.log(data)
+        const formData = new FormData();
+        formData.append('image', data.image[0])
+
+        fetch(img_hosting_url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(imgResponse => {
+            console.log(imgResponse)
+        })
     };
     console.log(errors);
+    console.log(img_hosting_token)
 
     return (
         <div className="w-full px-10">
@@ -29,8 +46,8 @@ const AddItem = () => {
                         <label className="label">
                             <span className="label-text">Category*</span>
                         </label>
-                        <select {...register("category", { required: true })} className="select select-bordered">
-                            <option disabled selected>Pick one</option>
+                        <select defaultValue="Pick One" {...register("category", { required: true })} className="select select-bordered">
+                            <option disabled >Pick One</option>
                             <option>Pizza</option>
                             <option>Soup</option>
                             <option>Salad</option>
@@ -50,7 +67,7 @@ const AddItem = () => {
                     <label className="label">
                         <span className="label-text">Recipe Details</span>
                     </label>
-                    <textarea {...register("recipe", { required: true })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                    <textarea {...register("recipe", { required: true })} className="textarea textarea-bordered h-24" placeholder="recipe details "></textarea>
                 </div>
 
                 {/* file input  */}
