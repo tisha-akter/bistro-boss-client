e0e97eef9f698b5ca1095a493d687adaedd410da
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-05 21:29:47 +0600

    create image url and newItem

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -6,7 +6,7 @@ const img_hosting_token=import.meta.env.VITE_Image_Upload_Token;
 const AddItem = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const img_hosting_url =`https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`
+    const img_hosting_url =`https://api.imgbb.com/1/upload?key=${img_hosting_token}`
 
     const onSubmit = data => {
        
@@ -20,7 +20,13 @@ const AddItem = () => {
         })
         .then(res => res.json())
         .then(imgResponse => {
-            console.log(imgResponse)
+            // console.log(imgResponse)
+            if(imgResponse.success){
+                const imgURL = imgResponse.data.display_url;
+                const {name, price, category, recipe} = data;
+                const  newItem = {name, price: parseFloat(price), category, recipe, image: imgURL}
+                console.log(newItem)
+            }
         })
     };
     console.log(errors);
@@ -53,6 +59,7 @@ const AddItem = () => {
                             <option>Salad</option>
                             <option>Dessert</option>
                             <option>Drinks</option>
+                            <option>Desi</option>
                         </select>
                     </div>
                     <div className="form-control w-full ml-4">
