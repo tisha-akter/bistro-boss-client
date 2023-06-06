e0f8601ee6a06c7247661fdc1de3d14421436a22
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-06 23:38:49 +0600

    saved new item to ther server and make api secure

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,11 +1,15 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const img_hosting_token=import.meta.env.VITE_Image_Upload_Token;
 
 const AddItem = () => {
+    
+    const  [axiosSecure] = useAxiosSecure();
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, reset} = useForm();
     const img_hosting_url =`https://api.imgbb.com/1/upload?key=${img_hosting_token}`
 
     const onSubmit = data => {
@@ -26,11 +30,27 @@ const AddItem = () => {
                 const {name, price, category, recipe} = data;
                 const  newItem = {name, price: parseFloat(price), category, recipe, image: imgURL}
                 console.log(newItem)
+
+                axiosSecure.post('/menu', newItem)
+                .then(data => {
+                    console.log('after posting new menu item', data.data)
+                    if(data.data.insertedId){
+                        reset()
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Item added successfully',
+                            showConfirmButton: false,
+                            timer: 1500
+                          })
+                    }
+                })
             }
         })
     };
-    console.log(errors);
-    console.log(img_hosting_token)
+
+    // console.log(errors);
+    // console.log(img_hosting_token)
 
     return (
         <div className="w-full px-10">
--- src/Pages/Dashboard/__make_secure.js
+++ src/Pages/Dashboard/__make_secure.js
@@ -1,5 +1,26 @@
 /** 
+ * 
+ * ______________
+ * BASIC
+ * _____________________
+ * 
  * 1. don't show the link to the who shouldn't see it 
  * . only show to the person/types of user who should see it 
  * 2. don't allow to visit the link by typing on the url
+ * use admin route that will check whether the user is admin or not
+ * if not admin then redirect to any other page. you could logout user and send them to the login page as well
+ * 
+ * 
+ * 
+ * 
+ * 
+ * --------------
+ * TO send data
+ * --------------
+ * 1. verify  jwt token (send authorization token in the header to the server) 
+ * if possile use axios to send jwt token by intercepting the request
+ * 
+ * 2. if it is an admin activity . Make sure only admin user is postin data by using verifyAdmin
+ * 
+ * 
 */
