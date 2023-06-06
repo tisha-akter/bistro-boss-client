00469db31186e50bbb25ba51707bf5322e6b74d4
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-07 00:07:57 +0600

    created manage item admin route and load all menu item

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -16,11 +16,12 @@ const Dashboard = () => {
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <div className="drawer-content ">
 
-                <Outlet></Outlet>
+                
 
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <Outlet></Outlet>
 
             </div>
             <div className="drawer-side bg-[#D1A054]">
--- src/Layout/ManageItems/ManageItems.jsx
+++ src/Layout/ManageItems/ManageItems.jsx
@@ -1,10 +1,74 @@
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import UseMenu from "../../hooks/UseMenu";
 
 
 const ManageItems = () => {
+    const [menu] = UseMenu();
     return (
         <div className="w-full">
             <SectionTitle heading="Manage All Items" subHeading="Hurry up"></SectionTitle>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th>Delete</th>
+                            <th>Update</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            menu.map(item => <tr key={item._id}>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">Hart Hagerty</div>
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Zemlak, Daniel and Leannon
+
+                                </td>
+                                <td>Purple</td>
+                                <td>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </td>
+                            </tr>)
+                        }
+
+
+
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
