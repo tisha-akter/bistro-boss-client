07a66c37a1d05af68db95bb476a974e363a90ba3
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-07 08:29:13 +0600

    Used stripe publihabale key

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./CheckOutForm";
 
+//todo: provide publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk);
 
 const Payment = () => {
     return (
         <div>
             <SectionTitle subHeading="Please Process" heading="PAYMENT"></SectionTitle>
             <h2 className="text-3xl">Taka Taka </h2>
+            <Elements stripe={stripePromise}>
+                <CheckOutForm></CheckOutForm>
+            </Elements>
         </div>
     );
 };
